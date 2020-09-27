1e8416be238803d07b7288454a54f1f723ba07fc
KD8BXP <kd8bxp@aol.com>
2020-09-27 00:35:00 -0400

    ham radio test code

---
+++ TBeam_LoRa_Receive_Display_all_packets/TBeam_LoRa_Receive_Display_all_packets.ino
@@ -0,0 +1,82 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+#include "images.h"
+
+#define SCK     5    // GPIO5  -- SX1278's SCK
+#define MISO    19   // GPIO19 -- SX1278's MISO
+#define MOSI    27   // GPIO27 -- SX1278's MOSI
+#define SS      18   // GPIO18 -- SX1278's CS
+#define RST     14   // GPIO14 -- SX1278's RESET
+#define DI0     26   // GPIO26 -- SX1278's IRQ(Interrupt Request)
+#define BAND    868E6
+
+SSD1306 display(0x3c, 21, 22);
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+//For this to work on a local level these parameters need to match
+int       loraSpreadingFactor = 9;
+int       loraCodingRate      = 5;
+int       loraTxPower         = 17;
+uint32_t  loraBandwidth       = 62500; //125E6;
+//This results in about 879bps if you believe this site https://unsigned.io/understanding-lora-parameters/
+//or maybe 488bps from this website https://www.rfwireless-world.com/calculators/LoRa-Data-Rate-Calculator.html
+
+void loraData(){
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0 , 15 , "Received "+ packSize + " bytes");
+  display.drawStringMaxWidth(0 , 26 , 128, packet);
+  Serial.println(packet);
+  display.drawString(0, 0, rssi); 
+  display.display();
+  Serial.println(rssi);
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
+
+void setup() {
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
+  
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println();
+  Serial.println("LoRa Receiver Callback");
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI0);  
+  if (!LoRa.begin(432300000)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+  //LoRa.onReceive(cbk);
+  LoRa.enableCrc(); //encable CRC checking - off by default
+  LoRa.setSpreadingFactor(loraSpreadingFactor);
+  LoRa.setCodingRate4(loraCodingRate);
+  LoRa.setSignalBandwidth(loraBandwidth);
+  LoRa.receive();
+  Serial.println("init ok");
+  display.init();
+  display.flipScreenVertically();  
+  display.setFont(ArialMT_Plain_10);
+   
+  delay(1500);
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { cbk(packetSize);  }
+  delay(10);
+}
